d0d3a9a23c4a63daef5a50f783587404f0eb1ee0
DangerKiddy <dropspawn3rfree@gmail.com>
2022-12-26 20:08:20 +0700

    Delete remotedetonator.lua

--- data/tdmp/TDMP/overrides/remotedetonator.lua
+++
@@ -1,9 +0,0 @@
-function detonatorInit()
-	TDMP_RegisterEvent("RemoteDetonator", function(json, steamid)
-
-	end)
-end
-
-Hook_AddListener("RegisterDetonatorShape", "TDMP_RemoteDetonator", function(shapeHandle)
-	-- return tostring(TDMP_RegisterNetworkShape(shape, data[5]))
-end)
